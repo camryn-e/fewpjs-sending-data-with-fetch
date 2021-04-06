263d89ac77ada7e316f663a1cb8a32c7ab88ec2d
camryn williams <williams.camryn.e@gmail.com>
2021-04-06 10:12:31 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(n, e){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: n,
+            email: e
+        })
+    })
+            .then(resp => resp.json())
+            .then((obj) => {
+                let objID = obj.id;
+                console.log("obj:", obj)
+                console.log("objID:", objID)
+                document.body.innerHTML = obj.id;
+            })
+            .catch(function(error) {
+                document.body.innerHTML = error.message;
+              });
+            
+}
+
+//document.addEventListener("DOMContentLoaded", submitData("jeff", "jeff@jeff"));
