4785b19928c101454cea77b26b5463860d254f06
camryn williams <williams.camryn.e@gmail.com>
2021-04-06 09:57:41 -0400

    commit tree 6644f27f61643b2d4f53636e0c19dcecd5efee27

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+
+function submitData(n, e){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: n,
+            email: e
+        })
+    })
+            .then(resp => resp.json())
+            .then((obj) => {
+                let objID = obj.id;
+                console.log("obj:", obj)
+                console.log("objID:", objID)
+                // let docElem = document.createElement("P")
+                // docElem.appendChild(objID);
+                // document.appendChild(docElem);
+                document.body.innerHTML = obj.id;
+            })
+            .catch(function(error) {
+                //alert("Bad things! Ragnarők!");
+                //console.log(error.message);
+                //let errorElem = document.createElement("p");
+                //errorElem.appendChild(error.message);
+                document.body.innerHTML = error.message;
+              });
+            
+}
+
